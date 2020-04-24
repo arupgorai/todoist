826f05caaaa89cbdbd4f690e997e71a7064ff444
Arup <arupgorai.js@gmail.com>
2020-04-24 16:44:41 +0530

    theme setup done using styled-component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom'
+import { ThemeProvider } from 'styled-components';
+
+import theme from './utils/theme';
+import GlobalStyles from './utils/global';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+render(
+    <ThemeProvider theme={theme}>
+        <>
+            <GlobalStyles />
+            <App />
+        </>
+    </ThemeProvider>,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
